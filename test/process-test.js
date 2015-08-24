e1a6e27ddae787baa7e0339489cfbd5e4a08ab34
Souren Papazian <souren@papazian.info>
2015-08-24 14:05:51 -0700

    test/data/valid_metrics.json checks the output in more detail

--- test/process-test.js
+++ test/process-test.js
@@ -12,14 +12,27 @@ describe('Crawl Processing', function() {
     it("Should return an object with expected properties", function() {
       var metrics = processCrawl(valid_row, false);
 
+      // crawl
       expect(metrics).to.have.property('crawl');
       expect(metrics.crawl).to.be.an('object');
 
+      expect(metrics.crawl).to.have.property('id');
+      expect(metrics.crawl.id).to.be.an('string');
+      expect(metrics.crawl).to.have.property('start');
+      expect(metrics.crawl.start).to.be.a('string');
+      expect(metrics.crawl).to.have.property('end');
+      expect(metrics.crawl.end).to.be.a('string');
+
+      // rippleds
       expect(metrics).to.have.property('rippleds');
       expect(metrics.rippleds).to.be.an('object');
+      expect(Object.keys(metrics.rippleds).length).to.equal(117);
 
+      // connections
       expect(metrics).to.have.property('connections');
       expect(metrics.connections).to.be.an('object');
+      expect(Object.keys(metrics.connections).length).to.equal(1942);
     });
+
   });
 });
